8cf2a184aac8a35d87e8e035eb7067156271688d
giuseppe <capassog97@gmail.com>
2024-03-22 17:05:56 +0100

    Working tailwindcss

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,13 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+    <h1 class="bg-purple-600"> { "Hello world" } </h1>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::Renderer::<App>::new().render();
 }
